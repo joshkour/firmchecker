8011e8b34db0b999d8e61dd34137219d60b6218f
joshkour <josh.kour@gmail.com>
2021-05-28 09:28:07 +1000

    Add files via upload

--- bootstrap/constants.php
+++ bootstrap/constants.php
@@ -8,6 +8,12 @@ define('ROLE_ID_CLIENT', 2);
 // User
 define('ROLE_ID_USER', 3);
 
+// Permissions
+define('PERMISSION_ID_EVENTS_ALL', 1);
+define('PERMISSION_ID_EVENTS_ORGAISATION', 2);
+define('PERMISSION_ID_EVENTS_USER', 3);
+
+// Event types
 define('EVENT_TYPE_REVIEWED', 1);
 define('EVENT_TYPE_THUMBS_UP', 2);
 define('EVENT_TYPE_THUMBS_DOWN', 3);
